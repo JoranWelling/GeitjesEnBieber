f7bf2162efd1fb1d25588a865d03a304e018545e
Joran-Welling <joran.welling@alliander.com>
2019-11-06 10:49:09 +0100

    Barts nonsens een beetje gefilterd

--- geitennaamgenerator.js
+++ geitennaamgenerator.js
@@ -1,16 +1,16 @@
-function maakGEITENNAAMHAHA(echteNaam){
+function maakGeitennaam(echteNaam){
     let aanhef = ["Koning", "Meneer", "Mevrouw", "Hertogin van"];
-    let achtervoegsel = ["Jr.", "de Stinkgeit", "Geitbek"];
+    let achtervoegsel = ["Jr.", "de Geit", "Geit"];
     let shuffledNaam = echteNaam.split('').sort(function () { return 0.5 - Math.random() }).join('');
     return aanhef.random() + " " + shuffledNaam + " " + achtervoegsel.random();
 }
-function maaknaamyoloboeie(){ 
+function maakNaam(){
     let naam = document.getElementById('name').value;
-    let geitennaam = maakGEITENNAAMHAHA(naam);
+    let geitennaam = maakGeitennaam(naam);
     document.getElementById('uitkomst').innerHTML = geitennaam;
 }
 window.addEventListener('load', function(){
-    document.getElementById('knop').addEventListener('click', maaknaamyoloboeie);
+    document.getElementById('knop').addEventListener('click', maakNaam);
 })
 
 
